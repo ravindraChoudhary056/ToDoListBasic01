c1c9052dd14e7624bd737efe98dbb07aa1924199
ravindraChoudhary056 <ravindrajakhadh017@gmail.com>
2025-12-25 15:03:25 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user");
 
 //  SIGN UP (POST)
 router.post("/signup", async (req, res) => {
